aaa7e42581d5f5d5b1ee56cd9c36d532b511ffa5
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-05-11 00:03:36 +0900

    引数に化学式を取りパースするまでを実装

--- minerals/app/Main.hs
+++ minerals/app/Main.hs
@@ -4,48 +4,9 @@ import Pauling
 import Type
 import Vis
 import Linear.V3
-
-cubeSize = 1.0
-vertex = cubeSize/2
-
-naSize = 0.15
-clSize = 0.30
-testModel1 = Cube cubeSize Wireframe blue
-testModel2 = [
-    Trans (V3 vertex vertex 0) $ Sphere naSize Solid blue,
-    Trans (V3 vertex (negate vertex) 0) $ Sphere naSize Solid blue,
-    Trans (V3 (negate vertex) vertex 0) $ Sphere naSize Solid blue,
-    Trans (V3 (negate vertex) (negate vertex) 0) $ Sphere naSize Solid blue,
-    Trans (V3 0 0 0) $ Sphere naSize Solid blue,
-
-    Trans (V3 vertex 0 (negate vertex)) $ Sphere naSize Solid blue,
-    Trans (V3 (negate vertex) 0 (negate vertex)) $ Sphere naSize Solid blue,
-    Trans (V3 0 (negate vertex) (negate vertex)) $ Sphere naSize Solid blue,
-    Trans (V3 0 vertex (negate vertex)) $ Sphere naSize Solid blue,
-
-    Trans (V3 vertex 0 vertex) $ Sphere naSize Solid blue,
-    Trans (V3 (negate vertex) 0 vertex) $ Sphere naSize Solid blue,
-    Trans (V3 0 (negate vertex) vertex) $ Sphere naSize Solid blue,
-    Trans (V3 0 vertex vertex) $ Sphere naSize Solid blue,
-
-
-    Trans (V3 vertex vertex vertex) $ Sphere clSize Solid yellow,
-    Trans (V3 vertex vertex (negate vertex)) $ Sphere clSize Solid yellow,
-    Trans (V3 (negate vertex) vertex vertex) $ Sphere clSize Solid yellow,
-    Trans (V3 (negate vertex) vertex (negate vertex)) $ Sphere clSize Solid yellow,
-    Trans (V3 vertex (negate vertex) vertex) $ Sphere clSize Solid yellow,
-    Trans (V3 vertex (negate vertex) (negate vertex)) $ Sphere clSize Solid yellow,
-    Trans (V3 (negate vertex) (negate vertex) vertex) $ Sphere clSize Solid yellow,
-    Trans (V3 (negate vertex) (negate vertex) (negate vertex)) $ Sphere clSize Solid yellow,
-    Trans (V3 vertex 0 0) $ Sphere clSize Solid yellow,
-    Trans (V3 (negate vertex) 0 0) $ Sphere clSize Solid yellow,
-    Trans (V3 0 (negate vertex) 0) $ Sphere clSize Solid yellow,
-    Trans (V3 0 vertex 0) $ Sphere clSize Solid yellow
-    ]
-
-testText = Text3d "NaCl" (V3 2 0.5 0) TimesRoman24 black
-
-testModels = VisObjects $ testModel1:testText:testModel2
+import CFParser
+import Basic (testModel)
+import System.Environment (getArgs)
 
 mineralWindow = defaultOpts{
     optWindowName = "Mineral Simulator",
@@ -56,4 +17,8 @@ mineralWindow = defaultOpts{
 
 main :: IO ()
 main = do
-    display mineralWindow testModels
+    (formula:_) <- getArgs
+    let elems = runParseCF formula
+    -- let elemsStr = (Text3d (show elems) (V3 1 2 0) TimesRoman10 red)
+    -- let testModels = VisObjects $ elemsStr:testModel
+    display mineralWindow testModel
